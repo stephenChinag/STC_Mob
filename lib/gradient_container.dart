b6467d91a6563843ba4e98dfb8a92babb5774cdd
StpehenChinag <stephenchinag@gmail.com>
2023-09-13 19:12:45 +0100

    gradient

---
+++ lib/gradient_container.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  // initializiation work
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 39, 13, 5),
+          Color.fromARGB(26, 5, 12, 40)
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      ),
+      child: const Center(
+        child: Text(
+          "Dante Morning",
+          style: TextStyle(
+              fontSize: 24.0,
+              color: Color.fromARGB(255, 241, 255, 246),
+              decoration: TextDecoration.underline),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: GradientContainer(),
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  GradientContainer() {
-    // initializiation work
-  }
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 39, 13, 5),
-          Color.fromARGB(26, 5, 12, 40)
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-      ),
-      child: const Center(
-        child: Text(
-          "Dante Morning",
-          style: TextStyle(
-              fontSize: 24.0,
-              color: Color.fromARGB(255, 241, 255, 246),
-              decoration: TextDecoration.underline),
-        ),
-      ),
-    );
-  }
 }
